2ce77ae27c8d917fa4799fce420ec5ff6a67f0c9
Bob O'Dell <bobodell@amazon.com>
2018-05-18 14:19:41 -0700

    DB update

--- config.php
+++ config.php
@@ -23,3 +23,19 @@ function config($key = '')
 
     return isset($config[$key]) ? $config[$key] : null;
 }
+
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'aa12a662qredpjn.czvw0awnaj4a.us-east-1.rds.amazonaws.com:3306');
+define('DB_USERNAME', 'bobodell');
+define('DB_PASSWORD', 'CSrds01!');
+define('DB_NAME', 'ebdb');
+ 
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+?>
