9e96fc17efdbd9ee7b6eab074c228cdb36c8fc06
Bob O'Dell <bobodell@amazon.com>
2019-11-20 14:47:16 -0800

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,10 +1,10 @@
 <?php
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
-define('DB_SERVER', 'aa12a662qredpjn.czvw0awnaj4a.us-east-1.rds.amazonaws.com:3306');
-define('DB_USERNAME', 'bobodell');
-define('DB_PASSWORD', 'CSrds01!');
-define('DB_NAME', 'ebdb');
+define('DB_SERVER', '');
+define('DB_USERNAME', '');
+define('DB_PASSWORD', '');
+define('DB_NAME', '');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
